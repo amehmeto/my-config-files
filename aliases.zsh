edcbc843b5b06c49d3085cc65fcff4dbd5039844
Arthur MEHMETOGLU (EXT) <arthur.mehmetoglu@ext.hermes.com>
2025-05-19 14:52:57 +0200

    add alias that combines gaa, gcmsg and gp

--- aliases.zsh
+++ aliases.zsh
@@ -80,3 +80,8 @@ alias jqdd="jq '.devDependencies' package.json"
 # source
 alias srcz="source ~/.zshrc"
 alias srcb="source ~/.bashrc"
+
+# Function to git add all, commit with a message, and push
+gacp() {
+  gaa && gcmsg "$1" && gp
+}
